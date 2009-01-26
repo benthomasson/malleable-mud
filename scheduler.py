08a75350965c72f4bc5508fef1c10960bc0adfa5
Ben Thomasson <ben.thomasson@gmail.com>
2009-01-25 20:36:40 -0500

    refactoring scheduler for test

--- scheduler.py
+++ scheduler.py
@@ -14,18 +14,21 @@ class Scheduler(actor.Actor):
         self.update = messages.Update()
         self.open = True
         self.world = world
+        self.steps = 0
 
     def addObject(self,id):
         self.objects.append(id)
 
     def close(self):
         self.open = False
+        self.task.kill()
 
     def run(self):
         lastTime = time.time()
         while self.open:
             if time.time() - lastTime > self.roundTime or not self.roundTime:
                 lastTime = time.time()
+                self.steps+=1
                 for id in self.objects:
                     self.world.sendMessage(id,self.update)
             stackless.schedule()
--- test/test.py
+++ test/test.py
@@ -22,6 +22,19 @@ class Test(unittest.TestCase):
         o.location = room.id
         self.assertEquals(o.location,room.id)
         self.assertEquals(room.objects,[o.id])
+
+    def testScheduler(self):
+        server = game.server.TestServer()
+        self.assertTrue(server.world)
+        self.assertTrue(server.scheduler)
+        self.assertEquals(server.scheduler.steps,0)
+        stackless.schedule()
+        self.assertEquals(server.scheduler.steps,1)
+        stackless.schedule()
+        self.assertEquals(server.scheduler.steps,2)
+        stackless.schedule()
+        self.assertEquals(server.scheduler.steps,3)
+        server.scheduler.close()
     
 
 if __name__ == '__main__':
